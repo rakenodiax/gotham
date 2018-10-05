52f805c70dcab4709eaa8ad1b8052241db4701b3
David Laban <alsuren@gmail.com>
2018-10-05 21:48:38 +0100

    actually async_handlers is commented out

--- examples/handlers/async_await/README.md
+++ examples/handlers/async_await/README.md
@@ -1,19 +1,28 @@
 # Async Request Handlers
 
 The idea of async handlers has already been introduced by the post_handler example in
-[Request Data](../request_data), and [Simple Async Handlers](../simple_async_handlers).
+[Request Data](../request_data), which waits for the POST body asyncronously, and resolves
+the response future once it has processed the body.
 
-This is a contrived example, that calls itself recursively over http, to produce a string of
-'z's of a requested length. This is not something that you would want to do in real life.
-That said, the techniques used should be transferrable to any code that makes calls
-to external services, and wants to do so without blocking other `Handler`s from running on the
-same thread while it is waiting for a response.
+This example contains a pair of endpoints that sleep for a number of seconds,
+in different ways. Note that we never call `std::thread::sleep` in this example,
+so none of our request handlers block the thread from handling other requests
+in parallel. Instead, in each case, we return a future, which will resolve when
+the requested time has elapsed, and cause Gotham to respond to the http request.
 
-You may notice that the code in this example takes a very similar structure to that of
-[Simple Async Handlers](../simple_async_handlers). This is on purpose: doing something
-useful with futures (like making a web request) does not need to be any more complicated
-than doing something simple like sleeping, if you manage to package it up into a nice
-simple API.
+The approach of using futures to track the status of long-running operations
+is significantly lower overhead than that of spawning a new thread per request.
+In our case, the long-running operations are sleeps, but in
+[Async Handlers](../async_handlers), the long-running operations are http
+requests.
+
+You will often find that most of the time that a web server spends dealing
+with a web request, it is waiting on another service (e.g. a database, or an
+external api). If you can track these operations using futures, you should end
+up with a very lightweight and performant web server. On the other hand, if you
+find yourself doing lots of CPU/memory intensive operations on the web server,
+then futures are probably not going to help your performance, and you might be
+better off spawning a new thread per request.
 
 ## Running
 
@@ -21,39 +30,24 @@ From the `examples/handlers/async_handlers` directory:
 
 ```
 Terminal 1:
-$ cargo run
-   Compiling handlers/async_handlers (file:///.../examples/handlers/async_handlers)
-    Finished dev [unoptimized + debuginfo] target(s) in 4.26 secs
-     Running `../handlers/async_handlers`
+   Compiling gotham_examples_handlers_simple_async_handlers v0.0.0 (file:///.../gotham/examples/handlers/simple_async_handlers)
+    Finished dev [unoptimized + debuginfo] target(s) in 8.19 secs
+     Running `.../gotham/target/debug/gotham_examples_handlers_simple_async_handlers`
 Listening for requests at http://127.0.0.1:7878
-series length: 2 starting
-series length: 1 starting
-series length: 1 finished
-series length: 1 starting
-series length: 1 finished
-series length: 2 finished
-loop length: 2 starting
-loop length: 1 starting
-loop length: 1 finished
-loop length: 1 starting
-loop length: 1 finished
-loop length: 2 finished
-parallel length: 2 starting
-parallel length: 1 starting
-parallel length: 1 finished
-parallel length: 1 starting
-parallel length: 1 finished
-parallel length: 2 finished
+sleep for 5 seconds once: starting
+sleep for 5 seconds once: finished
+sleep for one second 5 times: starting
+sleep for one second 5 times: finished
 
 Terminal 2:
-$ curl 'http://127.0.0.1:7878/series?length=2' && echo " = ok" || echo " = failed"
-zz = ok
-$ curl 'http://127.0.0.1:7878/loop?length=2' && echo " = ok" || echo " = failed"
-zz = ok
-$ curl 'http://127.0.0.1:7878/parallel?length=2' && echo " = ok" || echo " = failed"
-zz = ok
-
-
+$ curl 'http://127.0.0.1:7878/sleep?seconds=5'
+slept for 5 seconds
+$ curl 'http://127.0.0.1:7878/loop?seconds=5'
+slept for 1 seconds
+slept for 1 seconds
+slept for 1 seconds
+slept for 1 seconds
+slept for 1 seconds
 ```
 
 ## License
--- examples/handlers/async_await/src/main.rs
+++ examples/handlers/async_await/src/main.rs
@@ -9,238 +9,118 @@ extern crate mime;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate tokio_core;
+extern crate tokio;
 
-use futures::{future, stream, Future, Stream};
+use futures::{stream, Future, Stream};
+use std::time::{Duration, Instant};
 
 use hyper::StatusCode;
-#[cfg(not(test))]
-use hyper::{Client, Uri};
 
-use gotham::handler::{HandlerFuture, IntoHandlerError};
+use gotham::handler::{HandlerError, HandlerFuture, IntoHandlerError};
 use gotham::helpers::http::response::create_response;
 use gotham::router::builder::DefineSingleRoute;
 use gotham::router::builder::{build_simple_router, DrawRoutes};
 use gotham::router::Router;
 use gotham::state::{FromState, State};
 
-use tokio_core::reactor::Handle;
+use tokio::timer::Delay;
 
-type ResponseContentFuture = Box<Future<Item = Vec<u8>, Error = hyper::Error>>;
+type SleepFuture = Box<Future<Item = Vec<u8>, Error = HandlerError> + Send>;
 
 #[derive(Deserialize, StateData, StaticResponseExtender)]
 struct QueryStringExtractor {
-    length: i8,
+    seconds: u64,
 }
 
-/// This helper function does an HTTP GET, and returns the body as a `Vec`, so that it can be passed
-/// into `create_response` easily, and the example handlers can focus on the business logic.
-/// You may notice that the body collecting looks very similar to the POST example in
-/// `examples/handlers/request_data`.
-/// Notice also that this function needs a reference to a `Handle`. This is a handle to the event
-/// loop that the web request is being handled on. There are plans to stop needing it in later
-/// versions of tokio, so for now you can treat it as a bit of annoying bookkeeping and get on with
-/// your life.
+/// Sneaky hack to make tests take less time. Nothing to see here ;-).
 #[cfg(not(test))]
-fn http_get(handle: &Handle, url_str: &str) -> ResponseContentFuture {
-    let client = Client::new(handle);
-    let url: Uri = url_str.parse().unwrap();
-    let f = client.get(url).and_then(|response| {
-        response
-            .body()
-            .concat2()
-            .and_then(|full_body| Ok(full_body.to_vec()))
-    });
-
-    Box::new(f)
+fn get_duration(seconds: &u64) -> Duration {
+    Duration::from_secs(seconds.to_owned())
 }
-
-/// The other advantage of using a helper function is that you can easily patch it out for testing.
-/// You typically don't want to rely on external http services for your unit tests, because they
-/// will fail unexpectedly, and cause you to stop believing your unit tests when they fail.
-/// The subject of patching/mocking things out for test purposes is a big one, and this is just a
-/// toy example, so we just return success.
 #[cfg(test)]
-fn http_get(_handle: &Handle, _url_str: &str) -> ResponseContentFuture {
-    // We make the test version return something different from what a real view would, to make
-    // it easier to spot in the tests.
-    Box::new(future::ok(b"y".to_vec()))
+fn get_duration(seconds: &u64) -> Duration {
+    Duration::from_millis(seconds.to_owned())
 }
-
-/// Now we come to the business end of the example.
+/// All this function does is return a future that resolves after a number of
+/// seconds, with a Vec<u8> that tells you how long it slept for.
 ///
-/// This is a contrived example, that calls itself recursively over http, to produce a string of
-/// 'z's of length `length`. This is not something that you would want to do in real life.
-/// That said, the techniques used should be transferrable to any code that makes calls
-/// to external services, and wants to do so without blocking other Handlers from running on the
-/// same thread while it's waiting for a response.
+/// Note that it does not block the thread from handling other requests, because
+/// it returns a `Future`, which will be managed by the tokio reactor, and
+/// called back once the timeout has expired.
 ///
-/// Something to note about this example is that because we're accumulating results from one future
-/// to the next, our code drifts to the right. If you are using nightly, you can avoid this by
-/// using something like:
-/// https://github.com/alexcrichton/futures-await
-fn series_handler(mut state: State) -> Box<HandlerFuture> {
-    let length = QueryStringExtractor::take_from(&mut state).length;
-    println!("series length: {} starting", length);
-
-    // We have two base cases (`n = 0` and `n = 1`) and a block that recurses.
-    // Note that we pick a signature for our future that makes lives easier for our business logic,
-    // and then convert it into a `Box<HandlerFuture>` in the end.
-    let data_future: ResponseContentFuture = if length == 0 {
-        Box::new(future::ok(Vec::new()))
-    } else if length == 1 {
-        Box::new(future::ok(b"z".to_vec()))
-    } else {
-        // As discussed in `http_get`, we need to pass around a handle to the event loop
-        // we're running on. This is just book-keeping really, but here's how to do it:
-        let handle = Handle::borrow_from(&state).clone();
-
-        // These are the two URLs we're going to request. We're just splitting the length into
-        // two roughly equal parts and calling ourselves. In a real application, these might
-        // be external web apis or internal microservices.
-        let url_a = format!("http://127.0.0.1:7878/series?length={}", length / 2);
-        let url_b = format!(
-            "http://127.0.0.1:7878/series?length={}",
-            length / 2 + length % 2
-        );
-
-        // Here, we get the first URL, and then get the second URL, and then concatenate the
-        // two together. Notice that we have to move body_a into the second closure, and so our
-        // code drifts to the right.
-        let f = http_get(&handle, &url_a).and_then(move |mut body_a| {
-            http_get(&handle, &url_b).and_then(move |body_b| {
-                body_a.extend(body_b);
-                Ok(body_a)
-            })
+/// Vec<u8> is chosen because it is one of the things that you need to resolve
+/// a HandlerFuture and respond to a request.
+///
+/// Most things that you call to access remote services (e.g databases and
+/// web apis) can be coerced into returning futures that yield useful data,
+/// so the patterns that you learn in this example should be applicable to
+/// real world problems.
+fn sleep(seconds: u64) -> SleepFuture {
+    let when = Instant::now() + get_duration(&seconds);
+    let delay = Delay::new(when)
+        .map_err(|e| panic!("timer failed; err={:?}", e))
+        .and_then(move |_| {
+            Ok(format!("slept for {} seconds\n", seconds)
+                .as_bytes()
+                .to_vec())
         });
 
-        Box::new(f)
-    };
+    Box::new(delay)
+}
 
-    // Here, we convert the future from our handler into the form that Gotham expects.
-    // All we do is move `state` in, to return it, and convert any errors that we have.
-    Box::new(data_future.then(move |result| match result {
+/// This handler sleeps for the requested number of seconds, using the `sleep()`
+/// helper method, above.
+fn sleep_handler(mut state: State) -> Box<HandlerFuture> {
+    let seconds = QueryStringExtractor::take_from(&mut state).seconds;
+    println!("sleep for {} seconds once: starting", seconds);
+
+    // Here, we call our helper function that returns a future.
+    let sleep_future = sleep(seconds.clone());
+
+    // Here, we convert the future from `sleep()` into the form that Gotham expects.
+    // We have to use .then() rather than .and_then() because we need to coerce both
+    // the success and error cases into the right shape.
+    // `state` is moved in, so that we can return it, and we convert any errors
+    // that we have into the form that Hyper expects, using the helper from
+    // IntoHandlerError.
+    Box::new(sleep_future.then(move |result| match result {
         Ok(data) => {
-            let res = create_response(&state, StatusCode::Ok, Some((data, mime::TEXT_PLAIN)));
-            println!("series length: {} finished", length);
+            let res = create_response(&state, StatusCode::OK, (data, mime::TEXT_PLAIN));
+            println!("sleep for {} seconds once: finished", seconds);
             Ok((state, res))
         }
         Err(err) => Err((state, err.into_handler_error())),
     }))
 }
 
-/// This example uses a `future::Stream` to implement a `for` loop. This example only has two urls
-/// to call `http_get` on, but you can hopefully see how it is a useful pattern.
+/// This example uses a `future::Stream` to implement a `for` loop. It calls sleep(1)
+/// as many times as needed to make the requested duration.
 ///
-/// If any `http_get` call returns an error, then processing will stop, and the error will be
-/// returned.
-///
-/// https://github.com/alexcrichton/futures-await has a more readable syntax for this as
-/// well, if you are using nightly Rust.
+/// https://github.com/alexcrichton/futures-await has a more readable syntax for
+/// async for loops, if you are using nightly Rust.
 fn loop_handler(mut state: State) -> Box<HandlerFuture> {
-    let length = QueryStringExtractor::take_from(&mut state).length;
-    println!("loop length: {} starting", length);
+    let seconds = QueryStringExtractor::take_from(&mut state).seconds;
+    println!("sleep for one second {} times: starting", seconds);
 
-    // The structure is the same as `series_handler`, above.
-    let data_future: ResponseContentFuture = if length == 0 {
-        Box::new(future::ok(Vec::new()))
-    } else if length == 1 {
-        Box::new(future::ok(b"z".to_vec()))
-    } else {
-        let handle = Handle::borrow_from(&mut state).clone();
+    // Here, we create a stream of Ok(_) that's as long as we need, and use fold
+    // to loop over it asyncronously, accumulating the return values from sleep().
+    let sleep_future: SleepFuture = Box::new(stream::iter_ok(0..seconds).fold(
+        Vec::new(),
+        move |mut accumulator, _| {
+            // Do the sleep(), and append the result to the accumulator so that it can
+            // be returned.
+            sleep(1).and_then(move |body| {
+                accumulator.extend(body);
+                Ok(accumulator)
+            })
+        },
+    ));
 
-        let url_a = format!("http://127.0.0.1:7878/loop?length={}", length / 2);
-        let url_b = format!(
-            "http://127.0.0.1:7878/loop?length={}",
-            length / 2 + length % 2
-        );
-
-        // Here, we create a stream that contains our two URLs, and call fold to loop over all URLs
-        // and get the urls, concatenating the results into the accumulator (which starts off as the
-        // empty `Vec`).
-        let f =
-            stream::iter_ok(vec![url_a, url_b]).fold(Vec::new(), move |mut accumulator, url| {
-                // Do the http_get(), and append the result to the accumulator so that it can
-                // be returned.
-                http_get(&handle, &url).and_then(move |body| {
-                    accumulator.extend(body);
-                    Ok(accumulator)
-                })
-            });
-
-        Box::new(f)
-    };
-
-    Box::new(data_future.then(move |result| match result {
+    // This bit is the same as the bit in the first example.
+    Box::new(sleep_future.then(move |result| match result {
         Ok(data) => {
-            let res = create_response(&state, StatusCode::Ok, Some((data, mime::TEXT_PLAIN)));
-            println!("loop length: {} finished", length);
-            Ok((state, res))
-        }
-        Err(err) => Err((state, err.into_handler_error())),
-    }))
-}
-
-/// This example does the same thing as `series_handler`, but doesn't wait for the first request
-/// to return before starting the second. This approach is very tempting, but it is not recommended.
-///
-/// Problems with this approach include:
-/// * If both requests fail then you will get the error from whichever one happened to fail first,
-///   and the other error will be thrown on the floor.
-/// * This approach tends to cause spikes in resource usage across the different parts of your
-///   infrastructure, so a poorly written endpoint can amplify a single request into a storm,
-///   without any back-pressure when things are going slowly/failing.
-///
-/// If you try to  `curl 'http://127.0.0.1:7878/parallel?length=100'` then you will find that
-/// this example will cause the server to DoS itself with too many open tcp connections
-/// ("Too many open files"), but http://127.0.0.1:7878/series?length=100'` works just fine.
-///
-/// A piece of advice from the Erlang community (but which applies to any language with lightweight
-/// threads/promises) is:
-///
-///     "Use one parallel process to model each truly concurrent activity in the real world"
-///
-///     If there is a one-to-one mapping between the number of parallel processes and the number
-///     of truly parallel activities in the real world, the program will be easy to understand.
-///
-///     -- http://www.erlang.se/doc/programming_rules.shtml#REF34191
-///
-/// In summary:
-///     Don't do this at home kids. It is only included as a cautionary tale.
-///
-fn parallel_handler(mut state: State) -> Box<HandlerFuture> {
-    let length = QueryStringExtractor::take_from(&mut state).length;
-    println!("parallel length: {} starting", length);
-
-    // The structure is the same as in `series_handler`, above.
-    let data_future: ResponseContentFuture = if length == 0 {
-        Box::new(future::ok(Vec::new()))
-    } else if length == 1 {
-        Box::new(future::ok(b"z".to_vec()))
-    } else {
-        let handle = Handle::borrow_from(&mut state);
-
-        let url_a = format!("http://127.0.0.1:7878/parallel?length={}", length / 2);
-        let url_b = format!(
-            "http://127.0.0.1:7878/parallel?length={}",
-            length / 2 + length % 2
-        );
-
-        // Here, we get both urls in parallel, and then join the futures together at the end.
-        // See the docs of this function for a discussion of why this is a bad idea.
-        let f1 = http_get(handle, &url_a);
-        let f2 = http_get(handle, &url_b);
-
-        Box::new(f1.join(f2).and_then(|(mut body_a, body_b)| {
-            body_a.extend(body_b);
-            Ok(body_a)
-        }))
-    };
-
-    Box::new(data_future.then(move |result| match result {
-        Ok(data) => {
-            let res = create_response(&state, StatusCode::Ok, Some((data, mime::TEXT_PLAIN)));
-            println!("parallel length: {} finished", length);
+            let res = create_response(&state, StatusCode::OK, (data, mime::TEXT_PLAIN));
+            println!("sleep for one second {} times: finished", seconds);
             Ok((state, res))
         }
         Err(err) => Err((state, err.into_handler_error())),
@@ -251,19 +131,14 @@ fn parallel_handler(mut state: State) -> Box<HandlerFuture> {
 fn router() -> Router {
     build_simple_router(|route| {
         route
-            .get("/series")
+            .get("/sleep")
             .with_query_string_extractor::<QueryStringExtractor>()
-            .to(series_handler);
+            .to(sleep_handler);
         ;
         route
             .get("/loop")
             .with_query_string_extractor::<QueryStringExtractor>()
             .to(loop_handler);
-        ;
-        route
-            .get("/parallel")
-            .with_query_string_extractor::<QueryStringExtractor>()
-            .to(parallel_handler);
         ;
     })
 }
@@ -281,62 +156,27 @@ mod tests {
 
     use super::*;
 
-    fn assert_returns_ok(url_str: &str, expected_response: Vec<u8>) {
+    fn assert_returns_ok(url_str: &str, expected_response: &str) {
         let test_server = TestServer::new(router()).unwrap();
         let response = test_server.client().get(url_str).perform().unwrap();
 
-        assert_eq!(response.status(), StatusCode::Ok);
-        assert_eq!(response.read_body().unwrap(), expected_response);
-    }
-
-    // Tests for `series`
-
-    #[test]
-    fn series_returns_zero_zs_if_length_is_zero() {
-        assert_returns_ok("http://localhost/series?length=0", b"".to_vec());
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            &String::from_utf8(response.read_body().unwrap()).unwrap(),
+            expected_response
+        );
     }
 
     #[test]
-    fn series_returns_one_z_if_length_is_one() {
-        assert_returns_ok("http://localhost/series?length=1", b"z".to_vec());
+    fn sleep_says_how_long_it_slept_for() {
+        assert_returns_ok("http://localhost/sleep?seconds=2", "slept for 2 seconds\n");
     }
 
     #[test]
-    fn series_makes_two_http_gets_and_concatenates_the_responses_if_length_greater_than_one() {
-        assert_returns_ok("http://localhost/series?length=2", b"yy".to_vec());
-    }
-
-    // Tests for `loop`
-
-    #[test]
-    fn loop_returns_zero_zs_if_length_is_zero() {
-        assert_returns_ok("http://localhost/loop?length=0", b"".to_vec());
-    }
-
-    #[test]
-    fn loop_returns_one_z_if_length_is_one() {
-        assert_returns_ok("http://localhost/loop?length=1", b"z".to_vec());
-    }
-
-    #[test]
-    fn loop_makes_two_http_gets_and_concatenates_the_responses_if_length_greater_than_one() {
-        assert_returns_ok("http://localhost/loop?length=2", b"yy".to_vec());
-    }
-
-    // Tests for `parallel`
-
-    #[test]
-    fn parallel_returns_zero_zs_if_length_is_zero() {
-        assert_returns_ok("http://localhost/parallel?length=0", b"".to_vec());
-    }
-
-    #[test]
-    fn parallel_returns_one_z_if_length_is_one() {
-        assert_returns_ok("http://localhost/parallel?length=1", b"z".to_vec());
-    }
-
-    #[test]
-    fn parallel_makes_two_http_gets_and_concatenates_the_responses_if_length_greater_than_one() {
-        assert_returns_ok("http://localhost/parallel?length=2", b"yy".to_vec());
+    fn loop_breaks_the_time_into_one_second_sleeps() {
+        assert_returns_ok(
+            "http://localhost/loop?seconds=2",
+            "slept for 1 seconds\nslept for 1 seconds\n",
+        );
     }
 }
